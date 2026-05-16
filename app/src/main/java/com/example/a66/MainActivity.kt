7db524bf159c3caf7ce4a01727f65e9a72e1a78d
Antua <suleykin2005@mail.ru>
2026-05-16 20:59:14 +0300

    only ktor

--- app/src/main/java/com/example/a66/MainActivity.kt
+++ app/src/main/java/com/example/a66/MainActivity.kt
@@ -9,13 +9,11 @@ import com.example.a66.navigation.Navigation
 import androidx.navigation.compose.rememberNavController
 import com.example.a66.presentation.viewmodel.LaureateViewModel
 import com.example.a66.data.repository.KtorNobelPrizeRepositoryImpl
-import com.example.a66.data.repository.RetrofitNobelPrizeRepositoryImpl
 import com.example.a66.domain.usecase.FilterNobelPrizeUseCase
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // val filterUseCase = FilterNobelPrizeUseCase(RetrofitNobelPrizeRepositoryImpl())
         val filterUseCase = FilterNobelPrizeUseCase(KtorNobelPrizeRepositoryImpl())
         val vm = LaureateViewModel(filterUseCase)
         enableEdgeToEdge()
